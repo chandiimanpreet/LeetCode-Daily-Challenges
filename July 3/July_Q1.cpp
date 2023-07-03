5b0d55ac602bb83582982fcaf3406161dee3c3ff
chandiimanpreet <61014287+chandiimanpreet@users.noreply.github.com>
2023-07-03 12:45:45 +0530

    Add files via upload

---
+++ July 3/July_Q1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+        set<int> s1(nums1.begin(), nums1.end());
+        set<int> s2(nums2.begin(), nums2.end());
+        vector<int> v1,v2;
+
+        for(int num: s1) {
+            if(s2.count(num) == 0) 
+                v1.push_back(num);
+        }
+
+        for(int num: s2) {
+            if(s1.count(num) == 0) 
+                v2.push_back(num);
+        }
+        return {v1,v2};
+    }
+};
