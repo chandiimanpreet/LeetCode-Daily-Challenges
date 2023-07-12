4f16e946ea814334f64984a7dafe55b60b8f847e
chandiimanpreet <61014287+chandiimanpreet@users.noreply.github.com>
2023-07-12 14:19:08 +0530

    Add files via upload

---
+++ July 12/July_Q1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        set<string> s;
+
+        for(int i=0; i<paths.size(); i++) 
+            s.insert(paths[i][1]);
+
+        for(int i=0; i<paths.size(); i++) 
+            s.erase(paths[i][0]);
+
+        return *s.begin();        
+    }
+};
